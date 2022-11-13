af8a11d536e1d0f5a0c25f970f2a6ae353e45dce
ellaelisimo <gabriela.barsukova@gmail.com>
2022-11-13 14:49:14 +0200

    commit

--- Node.JS/express_cors/index.js
+++ Node.JS/express_cors/index.js
@@ -5,3 +5,24 @@ const PORT = 5002;
 const app = express();
 
 const cars = ["BMW", "VW", "Porsche"];
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/cars", (_, res) => {
+  res.send(cars);
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+//   res.send("OK");
+// });
+
+// app.post("/", (req, res) => {
+//   const car = req.body?.car;
+//   const newCar = [...cars, car];
+
+//   res.send(newCar);
+// });
