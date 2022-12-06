9c7f65e3d79706f4f70eaa6a4219f823128a40b4
ellaelisimo <gabriela.barsukova@gmail.com>
2022-12-06 21:57:18 +0200

    commit

--- Node.JS/CA-9/index.js
+++ Node.JS/CA-9/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 require("dotenv").config();
 
@@ -21,4 +21,45 @@ app.get("/categories", async (req, res) => {
     .find()
     .toArray();
   await connection.close();
+  return res.send(data);
 });
+
+app.get("/products", async (req, res) => {
+  const connection = await client.connect();
+  const data = await connection.db(DB).collection("products").find().toArray();
+  await connection.close();
+  return res.send(data);
+});
+
+app.get("/category-value", async (req, res) => {
+  try {
+    const connection = await client.connect();
+    const categories = await connection
+      .db(DB)
+      .collection("categories")
+      .find()
+      .toArray();
+
+    const products = await connection
+      .db(DB)
+      .collection("products")
+      .find()
+      .toArray();
+
+    const productsWithCategory = products.map((product) => {
+      const { title } = categories.find(
+        (category) => category._id.toString() === product.categoryId
+      );
+      return { ...product, category: title };
+    });
+
+    await connection.close();
+
+    res.send({ productsWithCategory }).end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+    throw Error(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
