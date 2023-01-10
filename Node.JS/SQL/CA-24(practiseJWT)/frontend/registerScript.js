dd031e868444d34dd4b62ac4b8357cce20301cd9
ellaelisimo <gabriela.barsukova@gmail.com>
2023-01-10 21:16:55 +0200

    commit

--- Node.JS/SQL/CA-24(practiseJWT)/frontend/registerScript.js
+++ Node.JS/SQL/CA-24(practiseJWT)/frontend/registerScript.js
@@ -1,5 +1,35 @@
-const registerForm = document.querySelector("form#registerForm");
+const registerForm = document.querySelector("#registerForm");
 
-registerForm.addEventListener("submit", (event) => {
+registerForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+
+  const registerEmailInput = document
+    .querySelector("#registerEmailInput")
+    .value.trim();
+  const registerPasswordInput = document
+    .querySelector("#registerPasswordInput")
+    .value.trim();
+
+  try {
+    const res = await fetch("http://localhost:5003/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: registerEmailInput,
+        password: registerPasswordInput,
+      }),
+    });
+    if (res.ok) {
+      registerForm.reset();
+      const user = await res.json();
+
+      localStorage.setItem("accessToken", user.token);
+      window.location.assign("./login.html");
+    }
+    if (!res.ok) {
+      return console.error("Can not register you.");
+    }
+  } catch (error) {
+    return console.error(error);
+  }
 });
